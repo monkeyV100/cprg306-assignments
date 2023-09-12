b67e846fb9f7d2104304ec9af47adcfe09fad4c2
Hye Won Hyun <Hye.Hyun@edu.sait.ca>
2023-09-12 13:49:06 -0600

    linked
    Link next is used, made jsx for student info function. created js for week2

---
+++ app/studentinfo/studentinfo.jsx
@@ -0,0 +1,13 @@
+
+function StudentInfo() {
+    return (
+        <div className="student-info"> 
+            <h1>Mac Hyun</h1>
+            <h1>CPRG 306 F</h1>
+            <a href="https://github.com/monkeyV100?tab=repositories">https://github.com/monkeyV100?tab=repositories</a>
+
+        </div>
+
+    );
+}
+export default StudentInfo;
---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+
+import StudentInfo from "../studentinfo/studentinfo.jsx";
+
+export default function ShoppingList(){
+    return(
+        <div>
+            <h1>My Shopping List</h1>
+            <StudentInfo></StudentInfo>
+        </div>
+    );
+}
